c126abb239bbe6ff65ee441b90a8d69f8bc6da5c
Ahmed Al-Zubidy <aalzubidy90@gmail.com>
2020-05-09 20:51:14 -0700

    Initialize with some settings

---
+++ app.js
@@ -0,0 +1,43 @@
+// Require
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const expressSanitizer = require('express-sanitizer');
+const path = require('path');
+
+// Import routes
+// const applicantsRoutes = require('./routes/applicants');
+// const programsRoutes = require('./routes/programs');
+
+// App Settings
+const port = 3001;
+app.engine('html', require('ejs').renderFile);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(expressSanitizer());
+
+// Index Route
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// User Routes
+// app.use(applicantsRoutes);
+
+// Programs Routes
+// app.use(programsRoutes);
+
+// All other routes
+app.get('*', function (req, res) {
+    res.render('notFound');
+});
+
+app.post('*', function (req, res) {
+    res.render('notFound');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});

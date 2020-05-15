142d8a022bd76241b28d0d029d400e66e75fae75
Ahmed Al-Zubidy <aalzubidy90@gmail.com>
2020-05-14 21:40:17 -0700

    Changed structure

---
+++ frontend-website/scripts/index.js
@@ -0,0 +1,121 @@
+const cons = require("consolidate");
+
+// Set the url based on the window URL and port - used across all calls
+var urlPrefix = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port;
+
+var app = angular.module('jsonToolkitApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "jsonpath.html"
+    })
+    .when("/jsonschema", {
+      templateUrl: "jsonschema.html"
+    })
+    .when('/*', {
+      templateUrl: "notFound.html"
+    });
+})
+
+app.controller("jsonPathController", function ($scope) {
+  $scope.autoRunLStatus = 'Auto Run Off';
+
+  $scope.test = 'JSON Tools Plus!';
+  $scope.savedPaths = [];
+
+  // Editor settings
+  var editorInputJSONPath = ace.edit("editorInputJSONPath");
+  editorInputJSONPath.setTheme("ace/theme/solarized_light");
+  editorInputJSONPath.session.setMode("ace/mode/json");
+
+  // Editor settings
+  var editorOutputJSONPath = ace.edit("editorOutputJSONPath");
+  editorOutputJSONPath.setTheme("ace/theme/solarized_light");
+  editorOutputJSONPath.session.setMode("ace/mode/json");
+
+  editorInputJSONPath.getSession().on('change', function () {
+    if (editorInputJSONPath.session.$annotations.length > 0) {
+      editorOutputJSONPath.setValue(JSON.stringify(editorInputJSONPath.session.$annotations));
+    } else {
+      editorOutputJSONPath.setValue('{}');
+    }
+  });
+
+
+  $scope.evaluateJSONPath = function evaluateJSONPath() {
+    const input = JSON.parse(editorInputJSONPath.session.getValue());
+    const jsonP = $scope.jsonP;
+    const result = JSONPath.JSONPath({
+      path: jsonP,
+      json: input
+    });
+    editorOutputJSONPath.setValue(JSON.stringify(result, null, 1), -1);
+  }
+
+  $scope.savePath = function savePath() {
+    $scope.savedPaths.push({
+      path: $scope.jsonP,
+      input: editorInputJSONPath.session.getValue(),
+      output: editorOutputJSONPath.session.getValue()
+    });
+  }
+
+  $scope.setAutoRun = function setAutoRun() {
+    if ($scope.autoRunLStatus === 'Auto Run Off') {
+      $scope.autoRunLStatus = 'Auto Run On';
+      $('#jsonP').on('input', () => {
+        $scope.evaluateJSONPath();
+      })
+    } else {
+      $('#jsonP').off();
+      $scope.autoRunLStatus = 'Auto Run Off';
+    }
+  }
+
+  $scope.loadSavedPath = function loadSavedPath(p, i, o) {
+    $scope.jsonP = p;
+    editorInputJSONPath.setValue(i, -1);
+    editorOutputJSONPath.setValue(o, -1);
+  }
+
+  $scope.switchOutputInput = function switchOutputInput() {
+    editorInputJSONPath.setValue(editorOutputJSONPath.session.getValue());
+    editorOutputJSONPath.setValue('{}');
+  }
+});
+
+app.controller("jsonSchemaController", function ($scope) {
+  $scope.jsonSchemaErrors = [];
+
+  // Editor settings
+  var editorInputJSONObject = ace.edit("editorInputJSONObject");
+  editorInputJSONObject.setTheme("ace/theme/solarized_light");
+  editorInputJSONObject.session.setMode("ace/mode/json");
+
+  // Editor settings
+  var editorInputJSONSchema = ace.edit("editorInputJSONSchema");
+  editorInputJSONSchema.setTheme("ace/theme/solarized_light");
+  editorInputJSONSchema.session.setMode("ace/mode/json");
+
+  $scope.validateJSON = function validateJSON() {
+    let schema = JSON.parse(editorInputJSONSchema.session.getValue());
+    let obj = JSON.parse(editorInputJSONObject.session.getValue());
+
+    var ajv = new Ajv();
+    var validate = ajv.compile(schema);
+    var valid = validate(obj);
+    if (!valid) {
+      $scope.jsonSchemaResults = "Invalid";
+      $scope.jsonSchemaErrors = validate.errors;
+    } else {
+      $scope.jsonSchemaResults = "Valid";
+    }
+  }
+
+  $scope.generateSchema = function generateSchema() {
+    let obj = JSON.parse(editorInputJSONObject.session.getValue());
+    editorInputJSONSchema.setValue(JSON.stringify(j2s(obj), null, 1), -1);
+  }
+
+});
